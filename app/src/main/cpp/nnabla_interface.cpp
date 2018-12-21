666ecc0aa6e85cc7b4b85a4a2c2909b49dd69ae6
Koichi Hatakeyama <kkhh777@gmail.com>
2018-12-22 02:46:25 +0900

    Cleanup jni interface.

--- app/src/main/cpp/nnabla_interface.cpp
+++ app/src/main/cpp/nnabla_interface.cpp
@@ -12,20 +12,8 @@
 
 #include "nnabla_interface.hpp"
 
-extern AAssetManager*	asset_manager;
-
 static NNablaInterface snn;
 
-extern "C" JNIEXPORT jstring
-JNICALL
-Java_net_seeeno_deeplearning_MainActivity_stringFromJNI(
-        JNIEnv *env,
-        jobject /* this */) {
-    std::string hello = "Hi from C++";
-    return env->NewStringUTF(hello.c_str());
-}
-
-//using nbla::Context;
 extern "C" JNIEXPORT void
 JNICALL
 Java_net_seeeno_deeplearning_MainActivity_nativeInitNeuralNetwork(
@@ -35,23 +23,9 @@ Java_net_seeeno_deeplearning_MainActivity_nativeInitNeuralNetwork(
         jstring network_name
         ) {
 
-    //using namespace nbla;
-    //const std::string nnp_file("network_file.nnp");
-    //const std::string input_bin("5.pgm");
-    //const std::string executor_name("Runtime");
-
-    //asset_manager = AAssetManager_fromJava(env, mgr);		// asset読み込みマネージャー
-    //AAsset*	_asset = AAssetManager_open(asset_manager, name, AASSET_MODE_BUFFER);
-
-    //nbla::Context cpu_ctx{{"cpu:float"}, "CpuCachedArray", "0"};
-    //nbla::Context ctx = cpu_ctx;
-
     const char *nnp_path = env->GetStringUTFChars(nnpPath, 0);
     const char *net_name = env->GetStringUTFChars(network_name, 0);
     snn.initialize(nnp_path, net_name);
-
-    // Create a Nnp object
-    //nbla::utils::nnp::Nnp nnp(ctx);
 
 }
 
