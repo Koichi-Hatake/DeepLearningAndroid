4c1b616bb71e88462af34e8dea293eca7e6b95d9
Koichi Hatakeyama <45664722+Koichi-Hatake@users.noreply.github.com>
2018-12-22 23:07:21 +0900

    Log output in native code.

--- app/src/main/cpp/nnabla_interface.hpp
+++ app/src/main/cpp/nnabla_interface.hpp
@@ -7,6 +7,7 @@
 #define DEEPLEARNING_NNABLA_INTERFACE_H
 
 #include <nbla_utils/nnp.hpp>
+#include <android/log.h>
 
 #include <cassert>
 #include <fstream>
@@ -22,6 +23,7 @@ private:
     std::shared_ptr<nbla::utils::nnp::Executor> mExecutor;
     nbla::Context mContext{{"cpu:float"}, "CpuCachedArray", "0"};
     std::string mPgmFile;
+    const char *TAG = "NativeCode";
 
     /** Read PGM image with MNIST shape.
 
@@ -88,10 +90,13 @@ public:
 
         nbla::CgVariablePtr x = mExecutor->get_data_variables().at(0).variable;
         uint8_t *data = x->variable()->cast_data_and_get_pointer<uint8_t>(mContext);
+        __android_log_print(ANDROID_LOG_VERBOSE, TAG, "Input Length: %d", len);
+        __android_log_print(ANDROID_LOG_VERBOSE, TAG, "Buffer Size: %d", x->variable()->size());
 
         // Copy pgm data array to input buffer.
         for (int i=0; i < len; i++) {
             data[i] = (uint8_t)pgm_data[i];
+            __android_log_print(ANDROID_LOG_VERBOSE, TAG, "%d: 0x%02x", i, data[i]);
         }
 
         mExecutor->execute();
